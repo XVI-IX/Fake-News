0a2df07cd70ffea42c0948cdc23d4e84f5075a87
XVI-IX <oladoja14@gmail.com>
2022-11-28 16:02:31 +0100

    Update pages/analysis

---
+++ pages/1_🔍️_Analysis.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from Home import df
+
+st.write("# **Dataframe**")
+x = st.slider("Rows", 0, 100, 10)
+st.write(df.head(x))
+
+values = [
+  df['label'][df['label'] == 'FAKE'].count(),
+  df['label'][df['label'] == 'REAL'].count()]
+labels = ['FAKE', 'REAL']
+
+
+fig = px.pie(
+  df, values = values,
+  color=labels,
+  names=labels, title="Percentage of News Classes",
+  hole = .5, color_discrete_map={
+    'REAL':"#0984e3",
+    'FAKE': "#d63031"
+  },
+  hover_data=None
+)
+fig.update_traces(textposition='inside')
+
+st.plotly_chart(fig)
+
+st.write(
+  "Since this appears to be a balanced dataset, it can be worked on without worrying about balancing it up.")
